1ed85c7d08d44ea22af182a84ed90a1db495811b
Thor Nolan <tnolan5764@gmail.com>
2019-02-27 08:22:48 -0800

    finished all the gameplay functions I need, still working on my css animations on button click

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,3 @@
-// on click animations: add the class "animated tada slow" to grandpa on loss using jQuery ("animated bounce slow" on win), and add "animated wobble fast" to pills on click
 
 // ================== GLOBALS =======================
 
@@ -33,41 +32,80 @@
 
         playerNumber = 0; 
 
-        // then add prescription and playerNumber to my page using jQuery
-        $("randomNumber").text(prescription);
-        $("yourNumber").text(playerNumber);
+        // then update prescription and playerNumber to my page using jQuery
+        $("#randomNumber").text(prescription);
+        $("#yourNumber").text(playerNumber);
     }
 
 
     // my functions for tracking on-click events and running conditionals to determine if the player has achieved a win or has exceeded the "prescription" and lost
 
     $("input[type='button']").click(function() {
-        // using a switch case here to determine which button was pressed and have the game respond accordingly
+        // using a switch case here to determine which button was pressed and then add points based on whichever random number has been assigned to that button
         switch (this.id) { 
-        case "orangeBottle":
-            $("#content").html("Content changed");
+
+          case "orangeBottle":
+            playerNumber = playerNumber + orangeBottle;
+            $("#yourNumber").text(playerNumber);
+                //conditional that runs my gameWin function on a win or my gameLoss function on a loss
+                if (playerNumber == prescription){
+                    gameWin();
+                }
+                else if (playerNumber > prescription){
+                    gameLoss();
+                }
             break;
-        case "greenBottle":
-            $("#content").html("Content changed again");
+
+          case "greenBottle":
+            playerNumber = playerNumber + greenBottle;
+            $("#yourNumber").text(playerNumber);
+                if (playerNumber == prescription){
+                    gameWin();
+                }
+                else if (playerNumber > prescription){
+                    gameLoss();
+                }
             break;
-        case "blueBottle":
-            $("#content").html("Content changed again");
+
+          case "blueBottle":
+            playerNumber = playerNumber + blueBottle;
+            $("#yourNumber").text(playerNumber);
+                if (playerNumber == prescription){
+                gameWin();
+            }
+            else if (playerNumber > prescription){
+                gameLoss();
+            }
             break;
-        case "purpleBottle":
-            $("#content").html("Content changed again");
+
+          case "purpleBottle":
+            playerNumber = playerNumber + purpleBottle;
+            $("#yourNumber").text(playerNumber);
+            if (playerNumber == prescription){
+                gameWin();
+            }
+            else if (playerNumber > prescription){
+                gameLoss();
+            }
             break;
         }
     });
 
 
-    // function to run if the player number is equal to the random number, and make wins go up by 1
-    function gameWin() {
+    // on click animations: add the class "animated tada slow" to grandpa on loss using jQuery, add "animated bounce slow" to grandpa on win, and add "animated wobble fast" to pills on click
 
+    // function to run if the player number is equal to the random number, make wins go up by 1, and add and then remove my animate.css classes
+    function gameWin() {
+        wins++;
+        $("#wins").text(wins);
+        reset();
     }
 
-    // function to run if the player number exceeds the random number, and make losses go up by 1
+    // function to run if the player number exceeds the random number, make losses go up by 1, and add and then remove my animate.css classes
     function gameLoss() {
-
+        losses++;
+        $("#losses").text(losses);
+        reset();
     }
 
 
