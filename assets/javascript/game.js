895ffceea98027f2237830e0111449dcb9ed0ab3
Thor Nolan <tnolan5764@gmail.com>
2019-02-27 07:54:57 -0800

    working on using a switch case to track my button presses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-// on click animations: add the class "animated tada slow" to grandpa  using jQuery, and add "animated wobble fast" to pills on click
+// on click animations: add the class "animated tada slow" to grandpa on loss using jQuery ("animated bounce slow" on win), and add "animated wobble fast" to pills on click
 
 // ================== GLOBALS =======================
 
@@ -18,14 +18,12 @@
     var wins = 0;
     var losses = 0;
 
-    // get my document elements by ID's using jQuery
-
 
 // ================== FUNCTIONS ======================
 
     // function to start/reset the game, displaying a random number at id #randomNumber and reassigning random values to each bottle and the prescription variables
 
-    function reset () {
+    function reset() {
         prescription = Math.floor(Math.random()*101+19);
 
         orangeBottle = Math.floor(Math.random()*11+1);
@@ -41,20 +39,43 @@
     }
 
 
-
-
     // my functions for tracking on-click events and running conditionals to determine if the player has achieved a win or has exceeded the "prescription" and lost
 
+    $("input[type='button']").click(function() {
+        // using a switch case here to determine which button was pressed and have the game respond accordingly
+        switch (this.id) { 
+        case "orangeBottle":
+            $("#content").html("Content changed");
+            break;
+        case "greenBottle":
+            $("#content").html("Content changed again");
+            break;
+        case "blueBottle":
+            $("#content").html("Content changed again");
+            break;
+        case "purpleBottle":
+            $("#content").html("Content changed again");
+            break;
+        }
+    });
 
 
-    // function to run if the player number is equal to the random number
+    // function to run if the player number is equal to the random number, and make wins go up by 1
+    function gameWin() {
+
+    }
 
     // function to run if the player number exceeds the random number, and make losses go up by 1
+    function gameLoss() {
 
+    }
 
 
 // ================== GAMEPLAY PROCESS ====================
 
-window.onload(function() {
-
-});
+    // resets values and also resets wins and losses if the window is loaded/reloaded
+    window.onload = function() {
+        wins = 0;
+        losses = 0;
+        reset();
+    };
